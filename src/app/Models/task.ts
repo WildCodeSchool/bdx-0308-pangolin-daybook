e7bbcd332c0e27fa3d7198edfd546f3f1aecc1c2
lePitch <pierre.henri.jautard@gmail.com>
2020-04-15 10:16:02 +0200

    complete task class

--- src/app/Models/task.ts
+++ src/app/Models/task.ts
@@ -1,2 +1,10 @@
 export class Task {
+
+  taskId: string;
+  taskChecked = false;
+  constructor(public title: string,  public importance: number) {}
+
+  checkedTask() {
+    this.taskChecked = !this.taskChecked;
+}
 }
