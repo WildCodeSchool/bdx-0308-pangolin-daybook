0d1831cd07fb7fdaf459970c496de0344d3fc3a0
Clement <clement.cheradame@free.fr>
2020-04-16 12:05:38 +0200

    Version final de la footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './Pages/dashboard/dashboard.component';
 import { HomeComponent } from './Pages/home/home.component';
+import { FooterComponent } from './CommonComponent/footer/footer.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: FooterComponent}
 ];
 
 @NgModule({
