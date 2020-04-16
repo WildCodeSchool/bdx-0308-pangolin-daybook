76dcc2deb233d8a4b79ca123940a521b6ce4df1b
Clement <clement.cheradame@free.fr>
2020-04-16 12:24:59 +0200

    Suppression des fichiers de la navbar qui pouvait potentiellement apportés des conflits lors du merge

--- src/app/CommonComponent/navbar/navbar.component.ts
+++ src/app/CommonComponent/navbar/navbar.component.ts
@@ -7,14 +7,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  @Input() homePage: boolean;
-  @Input() daybookNeeded: boolean;
-  @Input() userImgNeeded: boolean;
-
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.homePage);
 
   }
 
