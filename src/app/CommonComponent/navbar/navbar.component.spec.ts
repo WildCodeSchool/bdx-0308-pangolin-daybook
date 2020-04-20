f330c119d784f343ede812d868509ca779e257b0
Clement <clement.cheradame@free.fr>
2020-04-20 09:23:01 +0200

    Ajout d'un test pour le display ou non du titre dans la navbar

--- src/app/CommonComponent/navbar/navbar.component.spec.ts
+++ src/app/CommonComponent/navbar/navbar.component.spec.ts
@@ -1,10 +1,16 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, tick, fakeAsync } from '@angular/core/testing';
+
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { NavbarComponent } from './navbar.component';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 
-describe('NavbarComponent', () => {
+
+fdescribe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +22,21 @@ describe('NavbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should items array is created', () => {
+    expect(component.items).toBeTruthy();
+  });
+
+  it('should find the <h1> with fixture.debugElement.nativeElement)', () => {
+    component.homePage = false;
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('h1'))).toBeTruthy();
   });
 });
