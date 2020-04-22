82ca9e7772d86eab96d72e69607f633cf9655546
lePitch <pierre.henri.jautard@gmail.com>
2020-04-22 16:46:46 +0200

    tried to add prime module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { DashboardComponent } from './Pages/dashboard/dashboard.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { FooterComponent } from './CommonComponent/footer/footer.component';
 import { CreateDaybookComponent } from './Pages/create-daybook/create-daybook.component';
+import { ValidDaybookComponent } from './Pages/valid-daybook/valid-daybook.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'daybook', component: CreateDaybookComponent},
+  {path: 'valid', component: ValidDaybookComponent},
   {path: '**', component: HomeComponent}
 ];
 @NgModule({
