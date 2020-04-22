aa53dfd4e7f2c2eec43ea43f44c39a42967d70f2
lePitch <pierre.henri.jautard@gmail.com>
2020-04-22 10:12:40 +0200

    tests fixed

--- src/app/Models/daybook.spec.ts
+++ src/app/Models/daybook.spec.ts
@@ -1,7 +1,8 @@
 import { Daybook } from './daybook';
+import { Task } from './task';
 
 describe('Daybook', () => {
   it('should create an instance', () => {
-    expect(new Daybook()).toBeTruthy();
+    expect(new Daybook('theme', new Date(), [new Task('test', 1)] )).toBeTruthy();
   });
 });
