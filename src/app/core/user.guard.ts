5e58b882e85ff811ee8e5bd38f1aa6c78de1cddf
Bryan Even <bryaneven@gmx.fr>
2020-05-12 14:59:37 +0200

    fix guard

--- src/app/core/user.guard.ts
+++ src/app/core/user.guard.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from '../shared/user.service';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { User } from '../Models/user';
 
 @Injectable({
@@ -15,9 +15,7 @@ export class UserGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       if (localStorage.getItem('userToken')) {
-      return this.userService.getMe().pipe(map((user: User) => {
-          return true;
-        }));
+      return this.userService.getMe().pipe(map((user: User) => true), catchError((error) => of(false)));
       } else {
         this.router.navigateByUrl('/');
         return false;
